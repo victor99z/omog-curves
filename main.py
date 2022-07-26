7777b2225863c519e0f2133ccec668bba978fe16
Victor B <victor_luizbernardes5@outlook.com>
2022-07-25 23:14:55 -0300

    g1 + g2 = stonksg

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import pygame
 from Bspline import Bspline
 from Bezier import Bezier
+import math
 
 pygame.init()
 pygame.display.set_caption("Trabalho OMOG")
@@ -8,6 +9,7 @@ display = pygame.display.set_mode((1280, 720), pygame.RESIZABLE)
 clock = pygame.time.Clock()
 
 color = ['red', 'orange', 'green', 'cyan', 'white', 'yellow', 'blue', 'purple', 'grey']
+
 
 def draw_c0():
     x1, y1 = bezier.control_points[-1]
@@ -20,20 +22,42 @@ def draw_c0():
         x, y = bspline.control_points[i]
         bspline.control_points[i] = (x + coef_x, y + coef_y)
 
-    draw_c1()
-
     print(f'c1: \n {bezier.control_points} {bspline.control_points} {coef_x, coef_y}')
 
 
 def draw_c1():
-    v1,v2 = bezier.control_points[-1], bezier.control_points[-2] # (x,y), (x,y)
-    j1,j2 = bspline.control_points[1], bspline.control_points[2] # (x,y), (x,y)
+    draw_c0()
+    v1, v2 = bezier.control_points[-1], bezier.control_points[-2]  # (x,y), (x,y)
+    j1, j2 = bspline.control_points[0], bspline.control_points[1]  # (x,y), (x,y)
 
-    vetor_bezier, vetor_bspline = (abs(v2[0] - v1[0]), abs(v2[1] - v1[1])), (abs(j1[0] - j2[0]), abs(j1[1] - j2[1]))
+    # v1 = B | v2 = A
+    # j1 ~ B | j2 = P2
+    vetor_bezier_normalizado = (v1[0] - v2[0]), (v1[1] - v2[1])  # AB
+    raiz = math.sqrt(
+        math.pow(vetor_bezier_normalizado[0], 2) + math.pow(vetor_bezier_normalizado[1], 2))  # magnitude do AB
+    vetor_bezier_normalizado = vetor_bezier_normalizado[0] / raiz, vetor_bezier_normalizado[
+        1] / raiz  # Faz o AB ficar |AB| = 1
+    vetor_bspline = (j2[0] - j1[0], j2[1] - j1[1])
+    raiz2 = math.sqrt(
+        math.pow(vetor_bspline[0], 2) + math.pow(vetor_bspline[1], 2))  # Magnitude do [0,1] do controle da BSPLINE
+    bspline.control_points[1] = j1[0] + vetor_bezier_normalizado[0] * raiz2, j1[1] + vetor_bezier_normalizado[1] * raiz2
 
-    print(vetor_bezier, vetor_bspline, sep=" | ")
+    # ponto de controle 1 da bspline = Ponto de controle 0 da bspline + ( vetor bezier normalizado * magnitude da bspline )
 
-    return [vetor_bezier,vetor_bspline]
+    print(vetor_bezier_normalizado, vetor_bspline, sep=" | ")
+
+    return [vetor_bezier_normalizado, vetor_bspline]
+
+
+def draw_c2():
+    draw_c0()
+    v1, v2 = bezier.control_points[-1], bezier.control_points[-2]  # (x,y), (x,y)
+    j1, j2 = bspline.control_points[1], bspline.control_points[2]  # (x,y), (x,y)
+
+    vetor_bezier = (v1[0] - v2[0]), (v1[1] - v2[1])
+
+    bspline.control_points[1] = bspline.control_points[0][0] + vetor_bezier[0], bspline.control_points[0][1] + vetor_bezier[1]
+
 
 '''
 
@@ -41,7 +65,7 @@ Bezier
 
 '''
 
-bezier = Bezier(control_points=[])
+bezier = Bezier(control_points=[(486, 252), (191, 103), (481, 57), (703, 190), (704, 366), (497, 582)])
 
 '''
 
@@ -49,9 +73,7 @@ B-spline
 
 '''
 
-
 bspline = Bspline(control_points=[])
-
 
 flag = True
 run = True
@@ -76,6 +98,16 @@ while run:
                 if len(bezier.control_points) == 6 and len(bspline.control_points) == 9:
                     if flag:
                         draw_c0()
+                        flag = False
+            if event.key == pygame.K_n:
+                if len(bezier.control_points) == 6 and len(bspline.control_points) == 9:
+                    if flag:
+                        draw_c1()
+                        flag = False
+            if event.key == pygame.K_m:
+                if len(bezier.control_points) == 6 and len(bspline.control_points) == 9:
+                    if flag:
+                        draw_c2()
                         flag = False
 
     display.fill(0)
@@ -102,8 +134,6 @@ while run:
     if len(bspline.control_points) == 9:
         # print(bspline.control_points, bspline.degree, bspline.knots)
         bspline.draw_bspline(display)
-
-    
 
     pygame.display.update()
 
